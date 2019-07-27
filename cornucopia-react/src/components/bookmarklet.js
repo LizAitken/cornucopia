5d0009af8d59b9257f98b8242a5f57bb797daad7
Liz Aitken <liz.a.aitken@gmail.com>
2019-07-27 17:19:04 -0400

    Added styling to bookmarklet in mobile and desktop

--- cornucopia-react/src/components/bookmarklet.js
+++ cornucopia-react/src/components/bookmarklet.js
@@ -15,8 +15,8 @@
     wrapper.appendChild(box);
 
     box.setAttribute('id', 'box');
-    box.style.width= '60%';
-    box.style.height= '400px';
+    box.style.width= '70%';
+    box.style.height= '420px';
     box.style.overflow= 'auto';
     box.style.zIndex= '101';
     box.style.borderRadius= '10px';
@@ -31,19 +31,22 @@
     topWrap.style.justifyContent='space-between';
     topWrap.style.backgroundColor='#313b67';
     topWrap.style.color='white';
+    topWrap.style.boxShadow= '0 0 20px rgba(0,0,0,0.3)';
     let topText = document.createElement('h1');
     topText.textContent = 'Cornucopia';
+    topText.style.padding = '8px';
     topWrap.appendChild(topText);
 
     let exit = document.createElement('p');
     exit.textContent ='X';
-    exit.style.border = 'solid 2px black';
-    exit.style.width ='20px';
-    exit.style.height = '20px';
+    exit.style.fontSize = '15px';
+    exit.style.cursor = 'pointer';
+    exit.style.padding = '8px';
     topWrap.appendChild(exit);
 
     let secondWrap = document.createElement('div');
     secondWrap.style.textAlign = 'center';
+    secondWrap.style.margin = '10px';
     box.appendChild(secondWrap);
 
     let wishListTitle = document.createElement('h2');
@@ -56,6 +59,7 @@
     let title1 = document.getElementById('title').textContent;
     let title = document.createElement('p');
     title.textContent = title1;
+    title.style.fontSize = '16px';
     function replaceSingleQuotes(stringInput) {
         let newStringInput = stringInput.replace(/'/g, "''");
         return newStringInput;
@@ -68,6 +72,8 @@
             let price = document.createElement('p');
             price.textContent = price1;
             price.style.textAlign = 'center';
+            price.style.fontSize = '21px';
+            price.style.fontWeight = 'bold';
             box.appendChild(price);
             let imageInputWrap = document.createElement('div');
             imageInputWrap.style.display = 'flex';
@@ -77,6 +83,7 @@
 
             let imageWrap  = document.createElement('div');
             imageWrap.style.objectFit='fill';
+            imageWrap.style.marginRight = '18px';
             imageInputWrap.appendChild(imageWrap);
             let image1 = document.querySelector('#landingImage');
             let image2 = image1.getAttribute('src');
@@ -92,6 +99,9 @@
             imageInputWrap.appendChild(inputsWrap);
             let labelQuantity = document.createElement('p');
             labelQuantity.textContent = 'How many do you need?';
+            labelQuantity.style.fontWeight = 'bold';
+            labelQuantity.style.marginTop = '10px';
+            labelQuantity.style.fontSize = '17px';
             inputsWrap.appendChild(labelQuantity);
 
             let inputWrap = document.createElement('p');
@@ -101,9 +111,12 @@
             inputQuantity.setAttribute('type', 'text');   
             inputQuantity.setAttribute('value', '1');
             inputsWrap.appendChild(inputQuantity);
-
+            
             let labelName = document.createElement('p');
             labelName.textContent = 'Non-Profit ID:';
+            labelName.style.fontWeight = 'bold';
+            labelName.style.marginTop = '10px';
+            labelName.style.fontSize = '17px';
             inputsWrap.appendChild(labelName);
 
             let ngoNameWrap = document.createElement('p');
@@ -117,6 +130,11 @@
 
             let submitButton = document.createElement('button');
             submitButton.textContent = 'Add to My Wish List';
+            submitButton.style.fontWeight = 'bold';
+            submitButton.style.margin = '5px';
+            submitButton.style.fontSize = '16px';
+            submitButton.style.border = '1px solid black';
+            submitButton.style.borderRadius = '15px';
             inputsWrap.appendChild(submitButton);
 
             const currentURL = window.location.href;  
@@ -140,24 +158,39 @@
                 newQuantity === 0 || NGOnameValue === 0 ? alert('Please enter the quantity and your non-profit ID!'): sendInfo(); wrapper.style.display='none';                
             });
         } catch(err) {
-            let price1 = document.querySelector('.a-section a-spacing-none pw-buy-feature');
+            box.style.height= '510px';
+            topText.style.padding = '4px';
+            title.style.fontSize = '14px';
+            let price1 = document.querySelector('#newPitchPriceWrapper_feature_div').textContent;
             let price = document.createElement('p');
-            price.textContent = price1;
             price.style.textAlign = 'center';
+            price.style.margin = '5px';
+            price.style.padding = '0';
+            price.style.fontSize = '16px';
+            price.style.fontWeight = 'bold';
+            function getThePeriod(stringInput) {
+                let newNumber = Number.parseFloat(stringInput).toFixed(2);
+                return newNumber;
+            }
+            getThePeriod(price1);
+            console.log(price1);
+            price.textContent = price1;
+            box.appendChild(price);
             let imageInputWrap = document.createElement('div');
             imageInputWrap.style.display = 'flex';
-            imageInputWrap.style.flexDirection = 'row';
-            imageInputWrap.style.justifyContent = 'center';
+            imageInputWrap.style.flexDirection = 'column';
+            imageInputWrap.style.justifyItems = 'center';
+            imageInputWrap.style.textAlign = 'center';
             box.appendChild(imageInputWrap);
 
             let imageWrap  = document.createElement('div');
             imageWrap.style.objectFit='fill';
             imageInputWrap.appendChild(imageWrap);
-            let image1 = document.querySelector('#landingImage');
+            let image1 = document.querySelector('#main-image');
             let image2 = image1.getAttribute('src');
             let image = document.createElement('img');
             image.setAttribute('src', image2);
-            image.style.margin= '15px';
+            image.style.margin= '12px';
             image.style.height= '12rem';
             image.style.maxWidth= '300px';
             image.style.objectFit= 'contain';
@@ -167,6 +200,7 @@
             imageInputWrap.appendChild(inputsWrap);
             let labelQuantity = document.createElement('p');
             labelQuantity.textContent = 'How many do you need?';
+            labelQuantity.style.fontWeight = 'bold';
             inputsWrap.appendChild(labelQuantity);
 
             let inputWrap = document.createElement('p');
@@ -178,6 +212,8 @@
             inputsWrap.appendChild(inputQuantity);
 
             let labelName = document.createElement('p');
+            labelName.style.marginTop = '5px';
+            labelName.style.fontWeight = 'bold';
             labelName.textContent = 'Non-Profit ID:';
             inputsWrap.appendChild(labelName);
 
@@ -192,6 +228,10 @@
 
             let submitButton = document.createElement('button');
             submitButton.textContent = 'Add to My Wish List';
+            submitButton.style.fontWeight = 'bold';
+            submitButton.style.margin = '5px';
+            submitButton.style.border = '1px solid black';
+            submitButton.style.borderRadius = '15px';
             inputsWrap.appendChild(submitButton);
 
             const currentURL = window.location.href;  
@@ -219,9 +259,7 @@
     getPrice(); 
 })())
 
-let title2 = title1.replace(/%20/g, ' ');
-#dp-container
-#productTitle
+
 
 
 
