8ea15f3920dbcd6070ae407950589b9c9395cbe7
Liz Aitken <liz.a.aitken@gmail.com>
2019-07-27 14:33:17 -0400

    Fixed bookmarklet mobile problem

--- cornucopia-react/src/components/bookmarklet.js
+++ cornucopia-react/src/components/bookmarklet.js
@@ -144,7 +144,76 @@
             let price = document.createElement('p');
             price.textContent = price1;
             price.style.textAlign = 'center';
-            alert(price1);
+            let imageInputWrap = document.createElement('div');
+            imageInputWrap.style.display = 'flex';
+            imageInputWrap.style.flexDirection = 'row';
+            imageInputWrap.style.justifyContent = 'center';
+            box.appendChild(imageInputWrap);
+
+            let imageWrap  = document.createElement('div');
+            imageWrap.style.objectFit='fill';
+            imageInputWrap.appendChild(imageWrap);
+            let image1 = document.querySelector('#landingImage');
+            let image2 = image1.getAttribute('src');
+            let image = document.createElement('img');
+            image.setAttribute('src', image2);
+            image.style.margin= '15px';
+            image.style.height= '12rem';
+            image.style.maxWidth= '300px';
+            image.style.objectFit= 'contain';
+            imageWrap.appendChild(image);
+
+            let inputsWrap = document.createElement('div');
+            imageInputWrap.appendChild(inputsWrap);
+            let labelQuantity = document.createElement('p');
+            labelQuantity.textContent = 'How many do you need?';
+            inputsWrap.appendChild(labelQuantity);
+
+            let inputWrap = document.createElement('p');
+            inputsWrap.appendChild(inputWrap);
+
+            let inputQuantity = document.createElement('input');
+            inputQuantity.setAttribute('type', 'text');   
+            inputQuantity.setAttribute('value', '1');
+            inputsWrap.appendChild(inputQuantity);
+
+            let labelName = document.createElement('p');
+            labelName.textContent = 'Non-Profit ID:';
+            inputsWrap.appendChild(labelName);
+
+            let ngoNameWrap = document.createElement('p');
+            inputsWrap.appendChild(ngoNameWrap);
+
+            let ngoName = document.createElement('input');
+            ngoName.setAttribute('type', 'text');
+            ngoName.setAttribute('placeholder', 'Please type your Non-Profit ID here');
+            ngoName.setAttribute('value', '0');
+            ngoNameWrap.appendChild(ngoName);
+
+            let submitButton = document.createElement('button');
+            submitButton.textContent = 'Add to My Wish List';
+            inputsWrap.appendChild(submitButton);
+
+            const currentURL = window.location.href;  
+            let NGOnameValue = Number(ngoName.value);
+            let newQuantity = Number(inputQuantity.value);
+            let encodedTitle = encodeURIComponent(title1);  
+            function sendInfo() { 
+                let url = `http://localhost:3000/donations/wish-list-item-entry?donation_name=${encodedTitle}&donation_cost=${price1}&donation_photo=${image2}&donation_amount=${newQuantity}&donation_store_name=${storeNameValue}&donation_receiver=${NGOnameValue}&store_link=${currentURL}`;
+                return fetch(url, {
+                    method: 'POST'
+                }).then(response => response.json());
+            }
+            exit.addEventListener('click', function(e) {
+                e.preventDefault.default;
+                wrapper.style.display='none';
+            });
+            submitButton.addEventListener('click', function(e) {
+                e.preventDefault;
+                NGOnameValue = Number(ngoName.value);
+                newQuantity = Number(inputQuantity.value); 
+                newQuantity === 0 || NGOnameValue === 0 ? alert('Please enter the quantity and your non-profit ID!'): sendInfo(); wrapper.style.display='none';                
+            });
         }
     }
     getPrice(); 
@@ -153,76 +222,7 @@
 let title2 = title1.replace(/%20/g, ' ');
 #dp-container
 #productTitle
-let imageInputWrap = document.createElement('div');
-    imageInputWrap.style.display = 'flex';
-    imageInputWrap.style.flexDirection = 'row';
-    imageInputWrap.style.justifyContent = 'center';
-    box.appendChild(imageInputWrap);
 
-    let imageWrap  = document.createElement('div');
-    imageWrap.style.objectFit='fill';
-    imageInputWrap.appendChild(imageWrap);
-    let image1 = document.querySelector('#landingImage');
-    let image2 = image1.getAttribute('src');
-    let image = document.createElement('img');
-    image.setAttribute('src', image2);
-    image.style.margin= '15px';
-    image.style.height= '12rem';
-    image.style.maxWidth= '300px';
-    image.style.objectFit= 'contain';
-    imageWrap.appendChild(image);
-
-    let inputsWrap = document.createElement('div');
-    imageInputWrap.appendChild(inputsWrap);
-    let labelQuantity = document.createElement('p');
-    labelQuantity.textContent = 'How many do you need?';
-    inputsWrap.appendChild(labelQuantity);
-
-    let inputWrap = document.createElement('p');
-    inputsWrap.appendChild(inputWrap);
-
-    let inputQuantity = document.createElement('input');
-    inputQuantity.setAttribute('type', 'text');   
-    inputQuantity.setAttribute('value', '1');
-    inputsWrap.appendChild(inputQuantity);
-
-    let labelName = document.createElement('p');
-    labelName.textContent = 'Non-Profit ID:';
-    inputsWrap.appendChild(labelName);
-
-    let ngoNameWrap = document.createElement('p');
-    inputsWrap.appendChild(ngoNameWrap);
-
-    let ngoName = document.createElement('input');
-    ngoName.setAttribute('type', 'text');
-    ngoName.setAttribute('placeholder', 'Please type your Non-Profit ID here');
-    ngoName.setAttribute('value', '0');
-    ngoNameWrap.appendChild(ngoName);
-
-    let submitButton = document.createElement('button');
-    submitButton.textContent = 'Add to My Wish List';
-    inputsWrap.appendChild(submitButton);
-
-    const currentURL = window.location.href;  
-    let NGOnameValue = Number(ngoName.value);
-    let newQuantity = Number(inputQuantity.value);
-    let encodedTitle = encodeURIComponent(title1);  
-    function sendInfo() { 
-        let url = `http://localhost:3000/donations/wish-list-item-entry?donation_name=${encodedTitle}&donation_cost=${price1}&donation_photo=${image2}&donation_amount=${newQuantity}&donation_store_name=${storeNameValue}&donation_receiver=${NGOnameValue}&store_link=${currentURL}`;
-        return fetch(url, {
-            method: 'POST'
-        }).then(response => response.json());
-    }
-    exit.addEventListener('click', function(e) {
-        e.preventDefault.default;
-        wrapper.style.display='none';
-    });
-    submitButton.addEventListener('click', function(e) {
-        e.preventDefault;
-        NGOnameValue = Number(ngoName.value);
-        newQuantity = Number(inputQuantity.value); 
-        newQuantity === 0 || NGOnameValue === 0 ? alert('Please enter the quantity and your non-profit ID!'): sendInfo(); wrapper.style.display='none';                
-    });
 
 
 
